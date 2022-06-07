fad0f175a6dead4fa568964807c7af8e1abb7af4
Jorge Gonzalez <jagonzalezf9@gmail.com>
2022-06-06 21:26:15 -0700

    css backup

---
+++ src/components/Login.js
@@ -0,0 +1,177 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Login = (props) => {
+    return (
+        <Container>
+            <Nav>
+                <a href="/login">
+                {/*  <img src="/images/login-background.png" alt="Logo-Svg" />  */}
+                </a>
+                <div>
+                    <ConnectNow>Connect Now</ConnectNow>
+                    <SignIn> Sign In</SignIn>
+                </div>
+            </Nav>
+            <Section>
+                <Hero>
+                    <h1> Where finding you a job is our priority!</h1>
+                    <img src="/images/background.jpg" alt="" />
+                </Hero>
+                <Form>
+                    <Google>
+                        <img src="/images/google.svg" alt="" />
+                        Sign in with Google
+                    </Google>
+                </Form>
+            </Section>
+        </Container>
+    )
+}
+
+
+// CSS STYLING
+
+//Using styled components to not do inline editing ( use styled components extension to ot see the styling as a string)
+
+const Container = styled.div`
+    padding: 0px;
+`;
+
+//Nav bar svg styling
+const Nav = styled.nav`
+    max-width: 95%;
+    margin: auto;
+    padding: 12px 0 16px;
+    display: flex;
+    align-items: center;
+    position: relative;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+
+    /*  & > a = anything inside the a tag do this { } */
+    & > a {
+        width: 150px;
+        height: 40px;
+        @media (max-width: 760px) {
+            padding: 0 5px;
+        }
+    }
+`;
+
+// Connect Now button styling (kinda our take on sign up)
+const ConnectNow = styled.a`
+    font-size: 20px;
+    padding: 10px 12px;
+    text-decoration: none;
+    border-radius: 25px;
+    color: grey;
+    margin-right: 12px;
+    &:hover{
+        background-color: white;
+        color: black;
+        text-decoration: none;
+    }
+
+`;
+
+const SignIn = styled.a`
+    box-shadow: inset 0 0 0 1px #0E87EA;
+    color: #0E87EA;
+    border-radius: 18px;
+    transition-duration: 300ms;
+    font-size: 20px;
+    font-weight: 800;
+    line-height: 60px;
+    padding: 5px 15px;
+    text-align: center;
+    background-color: #ffffff;
+    &:hover {
+        background-color: #0E87EA;
+        color: #ffffff;
+        text-decoration: none ;
+    }
+`;
+
+const Section = styled.section`
+    display: flex;
+    align-content: start;
+    min-height: 800px;
+    padding-bottom: 150px;
+    padding-top: 50px;
+    padding: 50px 0;
+    position: relative;
+    flex-wrap: wrap;
+    width: 100%;
+    max-width: 95%;
+    align-items: center;
+    margin: auto;
+    @media (max-width:768px) {
+        margin: auto;
+        min-height: 0px;
+    }
+`
+
+const Hero = styled.div`
+    width: 100%;
+    h1 {
+        margin: 0;
+        padding-bottom: 0;
+        width: 100%;
+        font-size: 50px;
+        color: #0E87EA;
+        font-weight: 200;
+        line-height: 80px;
+        @media (max-width: 768px) {
+            text-align: center;
+            font-size: 20px;
+            width: 100%;
+            line-height: 2;
+        }
+    }    
+    
+    img {
+        width: 1000px;
+        height: 800px;
+        border-radius: 500px;
+        right: -1px;
+        position: absolute;
+        bottom: 100px;
+        align-items: center;
+        @media (max-width: 768px) {
+            top: 230px;
+            width: initial;
+            position: initial;
+            height: initial;
+        }
+    }
+`
+
+const Form = styled.div`
+    margin-top: 800px;
+    width: 400px;
+    @media (max-width: 768px) {
+        margin-top: 20px;
+    }
+`;
+
+const Google = styled.button`
+    display: flex;
+    justify-content: center;
+    background-color: #fff;
+    align-items: center;
+    height: 50px;
+    width: 100%;
+    border-radius: 25px;
+    vertical-align: middle;
+    z-index: 0;
+    transition-duration: 200ms;
+    color: #0E87EA;
+    &:hover{
+        background-color: rgba(207,207,207,0.25);
+        color: rgba(0,0,0,0.75);
+    }
+`;
+
+export default Login;
---
+++ src/components/Rightside.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+const Rightside = (props) => {
+  return (
+    <Container>
+      <FollowCard>
+        <Title>
+          <h2>Add to your feed</h2>
+          <img src="/" alt="" />
+        </Title>
+      </FollowCard>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  grid-area: rightside;
+`;
+
+const FollowCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+  padding: 12px;
+`;
+
+const Title = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  width: 100%;
+  color:#0E87EA;
+`;
+
+
+
+export default Rightside;
